c8bd3278f995fd9590bbf38881386761bd8671cb
L0K1E <logeshkeyboader@gmail.com>
2022-01-13 00:54:17 -0800

    settings updated

--- core/settings.py
+++ core/settings.py
@@ -104,7 +104,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-CORS_ALLOWED_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
@@ -116,6 +116,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1:3000"
+# ]
